7cf818c2b0b03ee3b858456616457dbfb81e2f61
Max B <maksym.bykov@gmail.com>
2019-01-31 02:17:39 +0300

    adding dependencies and firebase init

---
+++ src/firebase/init.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDUYYgXJ7D6BrV-Ltmm0jpW4xLJR5jicd0",
+    authDomain: "vue-firebase-chat-01.firebaseapp.com",
+    databaseURL: "https://vue-firebase-chat-01.firebaseio.com",
+    projectId: "vue-firebase-chat-01",
+    storageBucket: "vue-firebase-chat-01.appspot.com",
+    messagingSenderId: "975653954355"
+  };
+  const firebaseApp = firebase.initializeApp(config);
+  firebaseApp.firestore().settings({ timestampsInSnapshots: true });
+
+  export default firebaseApp.firestore();
